74bd536b239c16a123c949cfeb507fe8ad013139
apoleon33 <simondumesny@gmail.com>
2024-10-22 17:25:10 +0200

    lil docstring

--- lib/util/display_loading.dart
+++ lib/util/display_loading.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+/// Interface to manage every widget that needs to handle a loading for any reason.
 abstract class DisplayLoading {
-  Widget buildLoading(BuildContext context, );
+  /// The content that need to be displayed while anything is loading.
+  /// Generally with a use of [Shimmer].
+  Widget buildLoading(BuildContext context);
 
+  /// The actual content to display once the content is loaded.
   Widget buildLoadingEnded(BuildContext context);
 }

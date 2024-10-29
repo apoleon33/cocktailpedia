f92983c8d6ca49ef74379320c24d91a71f07e511
apoleon33 <simondumesny@gmail.com>
2024-10-29 17:35:24 +0100

    Unit test (they doesn't work)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cocktailpedia/database/cocktails_db.dart';
+import 'package:cocktailpedia/util/unit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -26,5 +28,37 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  // Copilot
+  group('Cocktail class tests', () {
+    test('Cocktail properties', () {
+      final cocktail = CocktailDatabase.kurrantTina;
+
+      expect(cocktail.name, 'Kurrant Tiña');
+      expect(cocktail.ingredients.length, 5);
+      expect(cocktail.glassType.name, 'Whisky');
+      expect(cocktail.description, isNotEmpty);
+      expect(cocktail.recipe, contains('Préparez vos ingrédients'));
+    });
+
+    test('Cocktail ingredient quantities', () {
+      final cocktail = CocktailDatabase.kurrantTina;
+
+      expect(cocktail.ingredients[0].quantity.value, 4.0);
+      expect(cocktail.ingredients[0].quantity.unit, Unit.cl);
+      expect(cocktail.ingredients[4].quantity.value, 4);
+      expect(cocktail.ingredients[4].quantity.unit, Unit.wedge);
+    });
+
+    test('Cocktail ingredient types', () {
+      final cocktail = CocktailDatabase.kurrantTina;
+
+      expect(cocktail.ingredients[0].ingredient.name, 'Cachaça');
+      expect(cocktail.ingredients[1].ingredient.name, 'blackcurrant liqueur');
+      expect(cocktail.ingredients[2].ingredient.name, 'lime');
+      expect(cocktail.ingredients[3].ingredient.name, 'cane');
+      expect(cocktail.ingredients[4].ingredient.name, 'greenLime');
+    });
   });
 }

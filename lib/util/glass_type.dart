f53e28b31f6ca423778816bd2a5fa77bf482e857
apoleon33 <simondumesny@gmail.com>
2025-01-11 20:23:03 +0100

    [GlassType] more glass variety

--- lib/util/glass_type.dart
+++ lib/util/glass_type.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class GlassType {
+class GlassType implements Comparable<GlassType> {
   final String name;
 
   /// A short description of what is the glass, how it is used...
@@ -13,18 +13,31 @@ class GlassType {
       );
 
   /// A list of all existing glass types, or at least the ones with an icon associated.
-  static List<GlassType> get glassTypes => [
-        const AnyGlassType(),
-        const GlassType(name: "Cocktail"),
-        const GlassType(name: "Hurricane"),
-        const GlassType(name: "Whisky"),
-        const GlassType(name: "Mug"),
-      ];
+  static List<GlassType> get glassTypes {
+    List<GlassType> glassTypeList = [
+      const AnyGlassType(),
+      const GlassType(name: "Cocktail"),
+      const GlassType(name: "Hurricane"),
+      const GlassType(name: "Whisky"),
+      const GlassType(name: "Mug"),
+      const GlassType(name: "Wine"),
+      const GlassType(name: "Pint"),
+      const GlassType(name: "Margarita"),
+      const GlassType(name: "sling"),
+      const GlassType(name: "tasting"),
+    ];
+    // alphabetical order
+    glassTypeList.sort((a, b) => a.compareTo(b));
+    return glassTypeList;
+  }
 
   @override
   String toString() {
     return 'GlassType{name: $name, description: $description}';
   }
+
+  @override
+  int compareTo(GlassType other) => name.compareTo(other.name);
 }
 
 class AnyGlassType extends GlassType {
